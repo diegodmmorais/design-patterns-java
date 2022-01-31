bb2f59e3e60a1b6cd40ebd8466bb0b08574afe0f
Diego Morais <diego.dm.morais@gmail.com>
2022-01-31 17:14:16 -0300

    create exemple de singleton patterns

---
+++ src/test/java/com/lukeware/singletonpattern/SecurityLoginTest.java
@@ -0,0 +1,22 @@
+package com.lukeware.singletonpattern;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Diego Morais
+ */
+@DisplayName("Security login test")
+class SecurityLoginTest {
+
+  @Test
+  @DisplayName("Login Test")
+  void login() {
+    final var instance1 = SecurityLogin.getInstance();
+    final var instance2 = SecurityLogin.getInstance();
+    Assertions.assertDoesNotThrow(() -> instance1.login("admin", "admin"));
+    Assertions.assertDoesNotThrow(() -> instance1.login("admin", "admin"));
+    Assertions.assertEquals(instance1, instance2);
+  }
+}
