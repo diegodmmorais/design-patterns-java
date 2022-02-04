ac9f7b5114c1a2793c449d69df4d2197a5f69d3c
Diego Morais <diego.dm.morais@gmail.com>
2022-02-04 15:21:51 -0300

    correction builder pattern

--- structural-design-patterns/flyweight-pattern/src/main/java/com/lukeware/flyweight/entity/DeliveryLocationDataBuilder.java
+++ structural-design-patterns/flyweight-pattern/src/main/java/com/lukeware/flyweight/entity/DeliveryLocationDataBuilder.java
@@ -1,7 +1,5 @@
 package com.lukeware.flyweight.entity;
 
-import java.util.Objects;
-
 /**
  * @author Diego Morais
  */
@@ -9,21 +7,12 @@ public final class DeliveryLocationDataBuilder {
   private String zipCode;
   private String cyte;
 
-  private static DeliveryLocationDataBuilder deliveryLocationDataBuilder;
-
   private DeliveryLocationDataBuilder() {
     super();
   }
 
   public static DeliveryLocationDataBuilder builder() {
-    if (Objects.isNull(deliveryLocationDataBuilder)) {
-      synchronized (DeliveryLocationDataBuilder.class) {
-        if (Objects.isNull(deliveryLocationDataBuilder)) {
-          deliveryLocationDataBuilder = new DeliveryLocationDataBuilder();
-        }
-      }
-    }
-    return deliveryLocationDataBuilder;
+    return new DeliveryLocationDataBuilder();
   }
 
   public DeliveryLocationDataBuilder street(String zipCode) {
